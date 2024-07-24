1cfa52863baf74743a5e405533d91ae78101b30a
dev2 <dev2@gmail.com>
2024-07-24 08:22:30 +0530

    initial commit by divyansh

---
+++ SpringBootRestApp/src/main/java/com/nt/rest/ActorRestController.java
@@ -0,0 +1,20 @@
+package com.nt.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actor-api")
+public class ActorRestController {
+
+	
+	@GetMapping("/wish/{name}")
+	public ResponseEntity<String> wishMessage(String name){
+		return new ResponseEntity<String>("Good Morning"+name,HttpStatus.OK);
+	}
+	
+
+}
